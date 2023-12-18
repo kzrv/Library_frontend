75592ab8cbb2ff04f1b1166152355fbfe1e5fe6c
Kozyrev Vasilij <kzrvv@outlook.com>
2023-12-18 12:33:54 +0100

    fix1

--- src/book/RentBook.tsx
+++ src/book/RentBook.tsx
@@ -7,6 +7,7 @@ import "../Buttons.css";
 import "../Modals.css";
 import "react-datepicker/dist/react-datepicker.css";
 import {RentBookInfo} from "./RentBookInfo";
+import * as Console from "console";
 
 type RentBookProps = {
     book: BookModel;
@@ -22,14 +23,40 @@ function RentBook(props: RentBookProps) {
         setModalRentBookIsOpen(false)
     }, [])
 
-    const [names, setNames] = useState<string[]>([]);
-    const fetchUsernames = useCallback(() => {
+    const [names, setNames] = useState<string[]>([]);const fetchUsernames = useCallback(() => {
         axios.get("/api/app-users/getAllUsernamesWithoutLibrian")
-            .then(response => response.data)
-            .catch(error => console.error("GET Error: " + error))
-            .then(setNames)
-    }, [])
-    useEffect(fetchUsernames, [fetchUsernames])
+            .then(response => {
+                // Выполнить первое действие с данными, если необходимо
+                const data = response.data;
+
+                // Выполнить второе действие с данными, если необходимо
+                // Например, установить в состояние или выполнить другую логику
+                setNames(data);
+
+                return data;
+            })
+            .catch(error => {
+                // Обработка ошибки, если необходимо
+                console.error("GET Error: " + error);
+                throw error; // Прокидываем ошибку дальше, чтобы её можно было обработать в другом месте
+            })
+            .then(data => {
+                // Выполнить третье действие с данными, если необходимо
+                // Этот блок выполнится, независимо от успешного завершения или ошибки
+
+                console.log("Additional action with data:", data);
+            });
+    }, []);
+
+    useEffect(fetchUsernames, [fetchUsernames]);
+    useEffect(() => {
+        if (names.length > 0) {
+            const initialRentByUsername = names[0];
+            handleRentChangeUsername({
+                target: { value: initialRentByUsername }
+            } as React.ChangeEvent<HTMLSelectElement>);
+        }
+    }, [names]);
 
     function rentBookBy() {
         return <>
@@ -42,9 +69,9 @@ function RentBook(props: RentBookProps) {
     }
 
     const [rentBookInfo, setRentBookInfo] = useState<RentBookInfo>({
-        rentByUsername: "",
+        rentByUsername: names.length > 0 ? names[0] : '', // или null, если массив пуст
         rentUntil: new Date()
-    })
+    });
 
     const handleRentBook = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -57,9 +84,10 @@ function RentBook(props: RentBookProps) {
 
     const handleRentChangeUsername = (event: ChangeEvent<HTMLSelectElement>) => {
         setRentBookInfo({
-            ...rentBookInfo, "rentByUsername": event.target.value
-        })
-    }
+            ...rentBookInfo,
+            rentByUsername: event.target.value
+        });
+    };
 
     const handleRentChangeDate = React.useCallback((date: Date) =>
         setRentBookInfo({
@@ -81,6 +109,7 @@ function RentBook(props: RentBookProps) {
             }
         </>;
     }
+
 
     return <>
         <button className="button button-rent-book" type={"submit"} onClick={openRentBookModal}>Rent</button>
@@ -105,9 +134,15 @@ function RentBook(props: RentBookProps) {
                 <br/>
                 <label htmlFor="rentBy">RentBy:</label>
 
-                <select className="selector" value={rentBookInfo.rentByUsername} name="rentByUsername"
-                        id="rentByUsername"
-                        onChange={handleRentChangeUsername}>{rentBookBy()}</select>
+                <select
+                    className="selector"
+                    value={rentBookInfo.rentByUsername || ''}
+                    name="rentByUsername"
+                    id="rentByUsername"
+                    onChange={handleRentChangeUsername}
+                >
+                    {rentBookBy()}
+                </select>
                 <p>Rent until:</p>
                 <DatePicker selected={rentBookInfo.rentUntil} onChange={handleRentChangeDate} showTimeSelect
                             dateFormat="Pp"/>
